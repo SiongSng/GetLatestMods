96b1adfa3de3f1fbb8ffde19d98d9647c6a84eb8
菘菘 <rrt467778@gmail.com>
2021-06-01 20:41:42 +0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-GetLatestMods
+## GetLatestMods CurseForge模組列表器
+
+### 介紹
+這個程式可以讓你抓取指定時間內的CurseForge模組並且做成試算表格式(如下圖)  
+
+![img.png](img.png)
+
+### 下載
+如需自行建置可參考下方的建置教學，如果不想自行建置可直接下載已經建置好的檔案
+Windows: [點我下載](https://github.com/SiongSng/GetLatestMods/raw/main/Build/Windows/GetLatestMods.exe)  
+Linux: (請自行建置)  
+MacOS: (請自行建置)
+
+### 組態
+請在執行目錄底下新建 **config.json** 此檔案，並輸入以下內容，用來儲存設定。
+```json
+{
+  "GameVersion" : "1.16.5",
+  "PageSize": "100",
+  "Date": "2021-05-01T00:00:00.00Z>2021-06-01T00:00:00.00Z"
+}
+```
+GameVersion: Minecraft模組的版本號(如 1.16/1.12/1.17.1...)。  
+PageSize: 要檢索的模組數量。  
+Date: 檢索的時間範圍。
+### 建置
+請先安裝 [Node.js](https://nodejs.org/en/) 再執行以下操作。  
+提醒您:在不同平台建置時，請使用該平台的設備才能正常建置。
+```cmd
+npm install
+```
+Windows:
+```cmd
+pkg package.json -t win
+```
+Linux:
+```cmd
+pkg package.json -t linux
+```
+MacOS:
+```cmd
+pkg package.json -t mac
+```
+
+### 授權
+本著作係採用 [MIT](https://opensource.org/licenses/MIT) 授權條款授權。
+
+
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const CurseForge = require("mc-curseforge-api");
 const config = require(`${process.cwd()}/config.json`)  //config
 const xlsx = require('node-xlsx'); //試算表解析模塊
 const fs = require("fs");
-const options = {'!cols': [{ wch: 8 }, { wch: 25 }, { wch: 75 }, { wch: 60 }, { wch: 8 }, { wch: 25 }, { wch: 25 } ]};
+const options = {'!cols': [{wch: 8}, {wch: 25}, {wch: 75}, {wch: 65}, {wch: 60}, {wch: 8}, {wch: 25}, {wch: 25}]};
+const translate = require('@vitalets/google-translate-api');
 
 let Wdata = [{
     name: '照模組更新時間排序資訊小工具',
@@ -11,6 +12,7 @@ let Wdata = [{
             '搜索編號',
             '模組名稱',
             '模組敘述',
+            '模組敘述(機器翻譯)',
             '下載網址',
             '下載數量',
             '更新日期',
@@ -20,20 +22,27 @@ let Wdata = [{
 },
 ]
 CurseForge.getMods({sort: 2, pageSize: config.PageSize, gameVersion: config.GameVersion}).then((mods) => {
-    console.log("-- 照模組更新時間排序資訊小工具 由 菘菘#8663 製作 --\n正在抓取資料中，請稍後...\n提醒您，請不要過量使用此程式以免導致讀取 CurseForge　API 超速。")
+    console.log("-- 照模組更新時間排序資訊小工具 由 菘菘#8663 製作 --\n正在抓取資料中，請稍後...\n提醒您，請不要過量使用此程式以免導致API使用過量。")
 
     async function aaa() {
+        console.log("正在翻譯模組敘述中，請稍後...")
         for (let i = 0; i < mods.length; i++) {
             let data = JSON.parse(JSON.stringify(mods[i]))
             if (Date.parse(data.created) > Date.parse(config.Date.split(">")[0]) && Date.parse(data.created) < Date.parse(config.Date.split(">")[1])) {
-                Wdata[0].data[Wdata[0].data.length] = [i + 1, data.name, data.summary, data.url, data.downloads, data.updated, data.created];
+                let Translated;
+                await translate(data.summary, {to: 'zh-TW'}).then(res => {
+                    Translated = res.text;
+                }).catch(err => {
+                    console.error(err);
+                });
+                Wdata[0].data[Wdata[0].data.length] = [i + 1, data.name, data.summary, Translated, data.url, data.downloads, data.updated, data.created];
             }
-        console.log(data.logo.url)
+            //console.log(data.logo.url) 模組圖示下載網址
         }
     }
 
     aaa().then(() => {
-        let buffer = xlsx.build(Wdata,options);
+        let buffer = xlsx.build(Wdata, options);
         fs.writeFile('opt.xlsx', buffer, function (err) {
             if (err) {
                 if (err.toString().startsWith("Error: EBUSY: resource busy or locked")) {
