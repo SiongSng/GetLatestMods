7168209188963aa8f95a23ed2af4ced9492e1173
菘菘 <rrt467778@gmail.com>
2021-06-04 17:39:06 +0800

    Update

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const Excel = require('excel4node');
 const fs = require("fs");
 const path = require("path");
 const config = require(`${process.cwd()}/config.json`)  //config
-const options = {
-};
+const options = {};
 const translate = require('@vitalets/google-translate-api');
 let TranslationProgress = 0;
 let num = 0;
+
+function addImage() {
+
+}
 
 function delDir(path) { //資料夾/檔案迴圈刪除 程式碼來自:https://www.itread01.com/content/1541387043.html
     let files = [];
@@ -31,18 +34,15 @@ function delDir(path) { //資料夾/檔案迴圈刪除 程式碼來自:https://w
 
 let wb = new Excel.Workbook();
 let ws = wb.addWorksheet('模組資料表格', options);
-ws.column(1).setWidth(25);
-ws.column(1).setWidth(25);
-ws.column(3).setWidth(30);
-ws.column(4).setWidth(75);
-ws.column(5).setWidth(60);
-ws.column(6).setWidth(80);
-ws.column(8).setWidth(35);
-ws.column(9).setWidth(35);
 
-for (let i = 0; i < 10; i ++){
-    ws.row(i).setHeight(22.5)
-}
+ws.column(2).setWidth(30);
+ws.column(3).setWidth(75);
+ws.column(4).setWidth(60);
+ws.column(5).setWidth(80);
+ws.column(6).setWidth(10);
+ws.column(7).setWidth(35);
+ws.column(8).setWidth(35);
+
 
 let style = wb.createStyle({ //試算表格式
     font: {
@@ -50,15 +50,13 @@ let style = wb.createStyle({ //試算表格式
         size: 14,
     },
 });
-ws.cell(1, 1).string("模組圖示").style(style)
-ws.cell(1, 2).string("搜索編號").style(style)
-ws.cell(1, 3).string("模組名稱").style(style)
-ws.cell(1, 4).string("模組敘述").style(style)
-ws.cell(1, 5).string("模組敘述(機器翻譯)").style(style)
-ws.cell(1, 6).string("下載網址").style(style)
-ws.cell(1, 7).string("下載數量").style(style)
-ws.cell(1, 8).string("更新日期").style(style)
-ws.cell(1, 9).string("創建日期").style(style)
+ws.cell(1, 2).string("模組名稱").style(style)
+ws.cell(1, 3).string("模組敘述").style(style)
+ws.cell(1, 4).string("模組敘述(機器翻譯)").style(style)
+ws.cell(1, 5).string("下載網址").style(style)
+ws.cell(1, 6).string("下載數量").style(style)
+ws.cell(1, 7).string("更新日期").style(style)
+ws.cell(1, 8).string("創建日期").style(style)
 
 let dirPath = path.join(__dirname, "icon"); //暫存模組圖示的位置
 delDir(dirPath)
@@ -68,58 +66,65 @@ if (!fs.existsSync(dirPath)) {
 
 CurseForge.getMods({sort: 2, pageSize: config.PageSize, gameVersion: config.GameVersion}).then((mods) => {
 
-    async function aaa() {
-        console.log("正在翻譯模組敘述中，請稍後...")
-        for (let i = 0; i < mods.length; i++) {
-            let data = JSON.parse(JSON.stringify(mods[i]));
-            if (Date.parse(data.created) > Date.parse(config.Date.split(">")[0]) && Date.parse(data.created) < Date.parse(config.Date.split(">")[1])) {
-                num++
-                let Translated;
-                await translate(data.summary, {to: 'zh-TW'}).then(res => {
-                    console.log(`翻譯進度: ${TranslationProgress / num * 100}%`)
-                    TranslationProgress++
-                    Translated = res.text;
-                }).catch(err => {
-                    console.error(err);
-                });
-                for (let k = 0; k < 1; k++) {
+        async function Run() {
+            console.log("正在翻譯模組敘述中，請稍後...")
+            for (let i = 0; i < mods.length; i++) {
+                let data = JSON.parse(JSON.stringify(mods[i]));
+                if (Date.parse(data.created) > Date.parse(config.Date.split(">")[0]) && Date.parse(data.created) < Date.parse(config.Date.split(">")[1])) {
+                    num++
                     let stream = fs.createWriteStream(path.join(`./icon/${data.logo.url.toString().substr(43, 65)}`));
-                    await request(data.logo.url).pipe(stream).on("close", function (err) {
-                         ws.addImage({ //將模組圖片新增到試算表內
+                    request(data.logo.url).pipe(stream).on("close", function (err) {
+                        if (err) throw err;
+                        ws.row(num).setHeight(70 / 3);
+                        ws.column(1).setWidth(15 / 3);
+                        ws.addImage({
                             path: `./icon/${data.logo.url.toString().substr(43, 65)}`,
                             type: 'picture',
-                             position: {
-                                 type: 'oneCellAnchor',
-                                 from: {
-                                     col: 1,
-                                     colOff: 0,
-                                     row: i,
-                                     rowOff: 0,
-                                 },
-                             },
+                            position: {
+                                type: 'twoCellAnchor',
+                                from: {
+                                    col: 1,
+                                    colOff: 0,
+                                    row: num + 1,
+                                    rowOff: 0,
+                                },
+                                to: {
+                                    col: 2,
+                                    colOff: 0,
+                                    row: num + 2,
+                                    rowOff: 0,
+                                },
+                            },
                         });
+                    })
+                    let Translated;
+                    await translate(data.summary, {to: 'zh-TW'}).then(res => {
+                        console.log(`翻譯進度: ${TranslationProgress / num * 100}%`)
+                        TranslationProgress++
+                        Translated = res.text;
+                    }).catch(err => {
+                        console.error(err);
                     });
+                    ws.cell(num + 1, 2).string(data.name).style(style);
+                    ws.cell(num + 1, 3).string(data.summary).style(style);
+                    ws.cell(num + 1, 4).string(Translated).style(style);
+                    ws.cell(num + 1, 5).link(data.url).style(style);
+                    ws.cell(num + 1, 6).number(data.downloads).style(style);
+                    ws.cell(num + 1, 7).string(data.updated).style(style);
+                    ws.cell(num + 1, 8).string(data.created).style(style);
                 }
-                ws.cell(num + 1, 2).number(i + 1).style(style);
-                ws.cell(num + 1, 3).string(data.name).style(style);
-                ws.cell(num + 1, 4).string(data.summary).style(style);
-                ws.cell(num + 1, 5).string(Translated).style(style);
-                ws.cell(num + 1, 6).link(data.url).style(style);
-                ws.cell(num + 1, 7).number(data.downloads).style(style);
-                ws.cell(num + 1, 8).string(data.updated).style(style);
-                ws.cell(num + 1, 9).string(data.created).style(style);
             }
         }
-    }
 
-    aaa().then(() => {
-        console.log(`翻譯進度: 100%`)
-        wb.write('opt.xlsx', function (err) {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log("成功寫入試算表"); // Prints out an instance of a node.js fs.Stats object
-            }
+        Run().then(() => {
+            console.log(`翻譯進度: 100%`)
+            wb.write('opt.xlsx', function (err) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log("成功寫入試算表"); // Prints out an instance of a node.js fs.Stats object
+                }
+            });
         });
-    });
-});
+    }
+);
